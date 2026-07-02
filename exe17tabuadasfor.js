86d8cc3cec05ce4c0b55b15cd296c21da43ab809
Raul Marçola <raulmarcola@gmail.com>
2026-07-02 10:29:03 -0300

    Add files via upload

---
+++ exe17tabuadasfor.js
@@ -0,0 +1,7 @@
+for( let x = 1; x <= 10; x++){
+    for( let y = 0; y <= 10; y++){
+        let result = x * y
+        console.log(`${x} X ${y} = ${result}`)
+    }
+    console.log('---------------------------------------------')
+}
